1b9c73ff978f02b2edd7ac0859ae26ef2cb10295
jrafaaael <jrmd070102@gmail.com>
2023-05-31 10:48:29 -0400

    feat: handle optimistic updates with messages

--- src/chat/schema/message.schema.ts
+++ src/chat/schema/message.schema.ts
@@ -4,4 +4,5 @@ import { usernameSchema } from "src/users/schema/user.schema";
 export const newMessageSchema = z.object({
   message: z.string(),
   username: usernameSchema,
+  optimisticId: z.string(),
 });
